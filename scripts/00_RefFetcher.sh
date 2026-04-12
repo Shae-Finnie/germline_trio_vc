6dafd7f9c706f18359579b0d12652bb9ad9fac01
Seamus-Finnegen <shaethomasfinnie@gmail.com>
2026-04-12 11:23:00 -0700

    updated align dependencies and wrote 00_RefFetcher.sh to retrieve dbSNP and 1000/mills indels

--- scripts/00_RefFetcher.sh
+++ scripts/00_RefFetcher.sh
@@ -7,14 +7,31 @@
 
 # Main
 
-bcftools view -r chr17 -O z -o data/reference/dbsnp_146.hg38.vcf.gz \
-    "ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/dbsnp_146.hg38.vcf.gz"
+set -euo pipefail
 
-bcftools index -t data/reference/dbsnp_146.hg38.vcf.gz
+REF_DIR="data/reference"
+CHROM="chr17"
 
-bcftools view -r chr17 -O z -o data/reference/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz \
-    "ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz"
+mkdir -p "${REF_DIR}"
 
-bcftools index -t data/reference/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
+# Download full VCFs + indexes
+echo "[INFO downloading dbSNP]"
+wget -q -P "${REF_DIR}" "https://ftp.ncbi.nih.gov/snp/latest_release/VCF/GCF_000001405.40.gz"
+wget -q -P "${REF_DIR}" "https://ftp.ncbi.nih.gov/snp/latest_release/VCF/GCF_000001405.40.gz.tbi"
+
+wget -q -P "${REF_DIR}" "https://ddbj.nig.ac.jp/public/public-human-genomes/GRCh38/fasta/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz"
+wget -q -P "${REF_DIR}" "https://ddbj.nig.ac.jp/public/public-human-genomes/GRCh38/fasta/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi"
+echo "[INFO downloading Mills]"
+
+# Slice chr17 and index
+echo "[INFO] slicing ${CHROM} from dbSNP"
+bcftools view -r "${CHROM}" -O z -o "${REF_DIR}/dbsnp_155.hg38.chr17.vcf.gz" "${REF_DIR}/GCF_000001405.40.gz"
+bcftools index -t "${REF_DIR}/dbsnp_155.hg38.chr17.vcf.gz"
+echo "[INFO] dbSNP chr17 done"
+
+echo "[INFO] slicing ${CHROM} from Mills"
+bcftools view -r "${CHROM}" -O z -o "${REF_DIR}/Mills_and_1000G_gold_standard.indels.hg38.chr17.vcf.gz" "${REF_DIR}/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz"
+bcftools index -t "${REF_DIR}/Mills_and_1000G_gold_standard.indels.hg38.chr17.vcf.gz"
+echo "[INFO] Mills chr17 done"
 
 # Both need .tbi indexes, -t 
