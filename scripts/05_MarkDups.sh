4249d0149575de7df6e71ff2b89a23063a059626
Seamus-Finnegen <shaethomasfinnie@gmail.com>
2026-04-12 09:50:20 -0700

    Added core loop to 05_Markdups.sh

--- scripts/05_MarkDups.sh
+++ scripts/05_MarkDups.sh
@@ -20,8 +20,6 @@ MARKEDDUP_DIR="data/marked"
 SAMPLES=("HG005" "HG006" "HG007")
 ROLES=("proband" "father" "mother")
 
-THREADS=8
-
 # Dir setup
 
 mkdir -p "${METRICS_DIR}"
@@ -29,4 +27,22 @@ mkdir -p "${MARKEDDUP_DIR}"
 
 # Main 
 
+echo " ************************************** "
+echo " Step 5: Mark Duplicates with GATK"
+echo " Input     : ${ALIGNED_DIR}"
+echo " Output    : ${MARKEDDUP_DIR}"
+echo " Metrics   : ${METRICS_DIR}"
+echo " ************************************** "
 
+for i in "${!SAMPLES}"; do
+    sample="${SAMPLES[$i]}"
+    role="${ROLES[$i]}"
+
+    INPUT="${ALIGNED_DIR}/${sample}_sorted.bam"
+    OUTPUT="${MARKEDDUP_DIR}/${sample}_markdup.bam"
+    METRICS="${METRICS_DIR}/${sample}_markdup_metrics.txt"
+
+    echo ""
+    echo " ************************************** "
+    echo " Sample: ${sample} (${role})"
+    echo " ************************************** "
